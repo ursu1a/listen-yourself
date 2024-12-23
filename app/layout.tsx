3ad99aaf45c37000db26cb855d0a9b9b536ee951
angelika <angelika@cobbleweb.co.uk>
2024-12-23 15:16:47 +0200

    Basic elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
+import { Footer } from "@/components/footer";
 
 export const metadata: Metadata = {
   title: {
@@ -16,7 +17,7 @@ export const metadata: Metadata = {
   },
   description: siteConfig.description,
   icons: {
-    icon: "/favicon.ico",
+    icon: "/favicon.png",
   },
 };
 
@@ -44,20 +45,10 @@ export default function RootLayout({
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
             <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+            <main className="container mx-auto max-w-8xl pt-6 px-6 flex-grow">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-                title="nextui.org homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">NextUI</p>
-              </Link>
-            </footer>
+            <Footer />
           </div>
         </Providers>
       </body>
---
+++ components/achievements.tsx
@@ -0,0 +1,36 @@
+import { useMemo } from "react";
+import { subtitle } from "./primitives";
+
+export const Achievements = () => {
+  const data = useMemo(
+    () => [
+      {
+        title: "4.9",
+        text: "Lorem ipsum dolor sit amet",
+      },
+      {
+        title: "876K",
+        text: "Lorem ipsum dolor sit amet",
+      },
+      {
+        title: "No.1",
+        text: "Lorem ipsum dolor sit amet",
+      },
+    ],
+    []
+  );
+
+  return (
+    <div className="flex flex-col justify-center w-full lg:flex-row bg-secondary transform scale-75 opacity-0 animate-block-in py-2 lg:py-6 rounded-xl lg:rounded-3xl divide-y divide-default-400 lg:divide-y-0">
+      {data.map((item, index) => (
+        <div key={index} className="flex flex-col items-center gap-y-2 lg:gap-y-4 text-center text-white lg:text-start lg:items-start lg:w-1/4 px-3 py-3 lg:py-0">
+          <p className={subtitle({ className: "font-semibold " })}>
+            {item.title}
+          </p>
+          <hr className="hidden lg:block border-b-3 w-1/3" />
+          <p>{item.text}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
