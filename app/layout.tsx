4840d0d26a99b811c62e052b8a3a85a8fdfd1530
angelika <angelika@cobbleweb.co.uk>
2025-03-03 16:55:55 +0200

    Views fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export const viewport: Viewport = {
   width: "device-width",
   height: "device-height",
   initialScale: 1,
+  maximumScale: 1,
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
